a60403bd8b3bd007abca3597d9cf92f06811236f
N0812237 <samiyazuu@gmail.com>
2021-12-28 12:48:53 +0100

    initial commit
    finished minting, about, partners section. Roadmap TBD, footer TBD, theme and design changes TBD

---
+++ script.js
@@ -0,0 +1,11 @@
+async function discord() {
+    window.open('https://discord.gg/wksmQSRXWj', '_blank').focus();
+}
+
+async function twitter() {
+    window.open('https://twitter.com/NextNatureNFT', '_blank').focus();
+}
+
+async function ig() {
+    window.open('https://instagram.com', '_blank').focus();
+}
