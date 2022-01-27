5697b4d23006b33cab764357a6b7e2e42b32e937
Sami Ljimari <samiyazuu@gmail.com>
2022-01-27 22:56:00 +0100

    update

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 async function discord() {
-    window.open('https://discord.gg/wksmQSRXWj', '_blank').focus();
+    window.open('https://discord.gg/CNBmtxUvxe', '_blank').focus();
 }
 
 async function twitter() {
     window.open('https://twitter.com/NextNatureNFT', '_blank').focus();
 }
 
-async function ig() {
-    window.open('https://instagram.com', '_blank').focus();
+async function openSea() {
+    window.open('https://opensea.io/', '_blank').focus();
+}
+
+async function project() {
+    element = document.getElementById("project")
+    element.scrollIntoView({behavior: "smooth"}); // Object parameter
+}
+
+async function community() {
+    element = document.getElementById("community")
+    element.scrollIntoView({behavior: "smooth"}); // Object parameter
+}
+
+async function minting() {
+    element = document.getElementById("minting")
+    element.scrollIntoView({behavior: "smooth"}); // Object parameter
+}
+
+async function FAQ() {
+    element = document.getElementById("faq")
+    element.scrollIntoView({behavior: "smooth"}); // Object parameter
+}
+
+async function roadmap() {
+    element = document.getElementById("roadmap")
+    element.scrollIntoView({behavior: "smooth"}); // Object parameter
 }
